5f70b51b13778e0d954de6a1f80b9034597e64ea
James Conroy-Finn <james@logi.cl>
2016-04-18 20:31:39 +0100

    Better support for multiple monitors

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,27 @@
 import XMonad
+import XMonad.Actions.SpawnOn
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig
+import XMonad.Config.Desktop
+import qualified XMonad.StackSet as W
 
-main = xmonad $ defaultConfig
-  { borderWidth = 1
-  , terminal = "urxvt"
-  , normalBorderColor = "#cccccc"
-  , focusedBorderColor = "#cd8b00" }
+myTerminal = "urxvt"
+
+myWorkspaces = map show [1..9]
+
+myKeys = [ (otherModMasks ++ "M-" ++ [key], action tag)
+         | (tag, key) <- zip myWorkspaces "123456789"
+         , (otherModMasks, action) <- [ ("", windows . W.view)
+                                      , ("S-", windows . W.shift)
+                                      ]
+         ]
+
+myConfig =
+  desktopConfig { borderWidth = 1
+                , terminal = myTerminal
+                , normalBorderColor = "#444444"
+                , focusedBorderColor = "#cd8b00"
+                , workspaces = myWorkspaces }
+  `additionalKeysP` myKeys
+
+main = xmonad =<< xmobar myConfig
